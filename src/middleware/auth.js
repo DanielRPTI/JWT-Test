ec72bd8d00dc0feb3c693c0b3b08c6fbc169f320
DanielRPTI <drjunior988@hotmail.com>
2023-10-20 11:42:36 -0300

    feat: adicionando funções finais e linkando o middlware no nosso server.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,8 +3,8 @@ import jsonwebtoken from "jsonwebtoken";
 const jwt = jsonwebtoken();
 export const PRIVATE_KEY = '1010FFF'
 export const user = {
-  name: 'Solus App',
-  email: 'solus@unimedfranca.com.br'
+  name: 'Teste',
+  email: 'test@gmail.com'
 }
 
 export function tokenValited( req, res, next) {
@@ -13,7 +13,7 @@ export function tokenValited( req, res, next) {
   if(!token) return res.satatus(401).json({message:'Access denied. No token provided.'})
 
   try {
-    const payload = jwt.verify()(token, PRIVATE_KEY);
+    const payload = jwt.verify(token, PRIVATE_KEY);
     const userIdFromToken = typeof payload !== 'string' && payload.user;
 
     if(!user && !userIdFromToken){
--- src/server.js
+++ src/server.js
@@ -1,12 +1,48 @@
 import express from "express";
 import jsonwebtoken from "jsonwebtoken";
-
-
+import {user, PRIVATE_KEY, tokenValited} from "./middleware/auth.js"
 
 const api = express();
+api.use(express.json())
 
-api.get('/', (req, res) => {return res.send('Test')})
+api.get('/', (res, req) => res.status(200).json({
+  message: 'This is a Public Router...'
+}))
+
+api.get('/login', (req, res) => {
+  const [, hash] = req.headers.authorization?.split(' ') || [' ', ' ']
+  const [email, password] = Buffer.from(hash, 'base64').toString().split(':')
+
+  try {
+    const correctPassword = email === 'test@gmail.com' && password === '123456'
+
+    if(!correctPassword) return res.status(401).send('Password or E-mail incorrect!')
+
+    const token  = jsonwebtoken.sign(
+      {user: JSON.stringify(user)},
+      PRIVATE_KEY,
+      {expiresIn: '60m'}
+    )
+  
+    return res.status(200).json({data: {user, token} })
+  } catch (error) {
+    console.log(error)
+    return res.send(error)
+  }
+})
+
+api.use('*', tokenValited)
+
+api.get('/private', (req, res) =>{
+  const {user} =  req.headers
+  const currentUser = JSON.parse(user)
+  return res.status(200).json({
+    message: 'This is a PRIVATE router ...',
+    data: {
+      userLogged: currentUser
+    }
+  })
+})
 
 
-
-app.listen(3030);
+app.listen(3030 , () => console.log('Server is running...'));
